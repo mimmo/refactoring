d4f5db2c68f36ce226e9699b13f0f72ddc0300c4
domenico.marini <domenico.marini@mfgroup.ch>
2021-03-08 15:13:48 +0100

    feat: improving the design of existing code start

---
+++ src/main/java/package-info.java
@@ -0,0 +1,25 @@
+/**
+ * First exercise on refactoring from Fowler's book. Do not forget to write tests.
+ * If you don't already have our custom preferences, ask for that.
+ *
+ * Keep in mind the following shortcuts, they may be useful:
+ * 		- ctrl + 1 : help from eclipse, either to solve errors and to auto-complete 
+ * 					 (use it when writing a new method as well as to complete the variables names)
+ * 					 e.g. syso AND crtl + 1 => System.out.println();
+ * 					 e.g. test4 AND crtl + 1 => test method declaration
+ *
+ * 		- ctrl + e : to focus an already open class
+ * 		- ctrl + h : to search for strings in the workspace 
+ * 		- ctrl + shift + r : to open a resource  
+ * 		- ctrl + shift + t : to open a new java class 
+ * 		- ctrl + shift + g : to see who calls the method/class
+ * 		
+ *		- alt + shift + r : to rename variables/classes/packages 
+ * 		- alt + shift + s : to open the source menu - generate getter/setter and constructor
+ * 		- alt + shift + t : to open the refactor menu - moving out method/class
+ */
+/**
+ * @since 1.0
+ * @author MF Group
+ * @version 1.0
+ */
