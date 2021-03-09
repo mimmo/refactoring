4724f8a7e0ea1122b2a62835ee219d3cc938d346
domenico.marini <domenico.marini@mfgroup.ch>
2021-03-09 11:54:23 +0100

    refactor: small test refactoring

--- src/test/java/refactoring/CustomerTest.java
+++ src/test/java/refactoring/CustomerTest.java
@@ -2,82 +2,95 @@ package refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
 
+	private Customer _customer;
+	private Movie _movie, _newReleaseMovie, _childrensMovie;
+
+	@Before
+	public void setUp(){
+		_customer = new Customer("mario");
+		_movie = new Movie("The movie", 0);
+		_newReleaseMovie = new Movie("The movie", 1);
+		_childrensMovie = new Movie("The movie", 2);
+	}
+
 	@Test
 	public void considerCustomerWithoutRentals() {
-		Customer customer = new Customer("mario");
-		assertEquals(customer.statement(), "Rental Record for mario\nAmount owed is 0.0\nYou earned 0 frequent renter points");
+		assertEquals(
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"Amount owed is 0.0",
+				"You earned 0 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 	@Test
 	public void considerCustomerWithARegularMovieRental() {
-		Customer customer = new Customer("mario");
-		Movie movie = new Movie("The movie", 0);
-		customer.addRental(new Rental(movie, 1));
+		_customer.addRental(new Rental(_movie, 1));
 		assertEquals(
-			customer.statement(),
-			"Rental Record for mario\n"
-			+ "\tThe movie\t2.0\n"
-			+ "Amount owed is 2.0\n"
-			+ "You earned 1 frequent renter points");
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"\tThe movie\t2.0",
+				"Amount owed is 2.0",
+				"You earned 1 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 	@Test
 	public void considerCustomerWithTwoRegularMoviesRentals() {
-		Customer customer = new Customer("mario");
-		Movie movie = new Movie("The movie", 0);
-		Movie movie2 = new Movie("Another movie", 0);
-		customer.addRental(new Rental(movie, 1));
-		customer.addRental(new Rental(movie2, 1));
+		_customer.addRental(new Rental(_movie, 1));
+		_customer.addRental(new Rental(_movie, 1));
 		assertEquals(
-			customer.statement(),
-			"Rental Record for mario\n"
-			+ "\tThe movie\t2.0\n"
-			+ "\tAnother movie\t2.0\n"
-			+ "Amount owed is 4.0\n"
-			+ "You earned 2 frequent renter points");
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"\tThe movie\t2.0",
+				"\tThe movie\t2.0",
+				"Amount owed is 4.0",
+				"You earned 2 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 	@Test
 	public void considerCustomerWithANewReleaseMovieRental() {
-		Customer customer = new Customer("mario");
-		Movie movie = new Movie("The movie 2", 1);
-		customer.addRental(new Rental(movie, 1));
+		_customer.addRental(new Rental(_newReleaseMovie, 1));
 		assertEquals(
-			customer.statement(),
-			"Rental Record for mario\n"
-			+ "\tThe movie 2\t3.0\n"
-			+ "Amount owed is 3.0\n"
-			+ "You earned 1 frequent renter points");
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"\tThe movie\t3.0",
+				"Amount owed is 3.0",
+				"You earned 1 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 	@Test
 	public void considerCustomerWithANewReleaseMovieRentalTwoDaysLong() {
-		Customer customer = new Customer("mario");
-		Movie movie = new Movie("The new movie", 1);
-		customer.addRental(new Rental(movie, 2));
+		_customer.addRental(new Rental(_newReleaseMovie, 2));
 		assertEquals(
-			customer.statement(),
-			"Rental Record for mario\n"
-			+ "\tThe new movie\t6.0\n"
-			+ "Amount owed is 6.0\n"
-			+ "You earned 2 frequent renter points");
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"\tThe movie\t6.0",
+				"Amount owed is 6.0",
+				"You earned 2 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 	@Test
 	public void considerCustomerWithAChildrenMovieRental() {
-		Customer customer = new Customer("mario");
-		Movie movie = new Movie("The children animation movie", 2);
-		customer.addRental(new Rental(movie, 1));
+		_customer.addRental(new Rental(_childrensMovie, 1));
 		assertEquals(
-			customer.statement(),
-			"Rental Record for mario\n"
-			+ "\tThe children animation movie\t1.5\n"
-			+ "Amount owed is 1.5\n"
-			+ "You earned 1 frequent renter points");
+			_customer.statement(),
+			Stream.of("Rental Record for mario",
+				"\tThe movie\t1.5",
+				"Amount owed is 1.5",
+				"You earned 1 frequent renter points"
+			).collect(Collectors.joining("\n")));
 	}
 
 }
