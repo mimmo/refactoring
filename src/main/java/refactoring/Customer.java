fc3d230b5a1840103feec819d1b3ef1b2722dd95
domenico.marini <domenico.marini@mfgroup.ch>
2021-03-09 13:22:11 +0100

    refactor: extraction of the totals

--- src/main/java/refactoring/Customer.java
+++ src/main/java/refactoring/Customer.java
@@ -22,24 +22,40 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 
 		while (rentals.hasNext()) {
 			Rental each = rentals.next();
 
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+
+	private double getTotalCharge() {
+		double result = 0;
+		Iterator<Rental> rentals = _rentals.iterator();
+		while (rentals.hasNext()) {
+			Rental each = rentals.next();
+			result += each.getCharge();
+		}
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Iterator<Rental> rentals = _rentals.iterator();
+		while (rentals.hasNext()) {
+			Rental each = rentals.next();
+			result += each.getFrequentRenterPoints();
+		}
 		return result;
 	}
 }
